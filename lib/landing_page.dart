e6b675d34fa0e56834bc6a146976838055fc765c
mehmet berkay <atasoy182@gmail.com>
2021-03-27 14:13:23 +0300

    Get it paketi ile singleton yapısına geçildi, repository katmanı için hazırlık yapılıyor.

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -2,22 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_lovers/home_page.dart';
 import 'package:flutter_lovers/model/app_user_model.dart';
 import 'package:flutter_lovers/services/auth_base.dart';
+import 'package:flutter_lovers/services/firebase_auth_service.dart';
 import 'package:flutter_lovers/sign_in_page.dart';
 
+import 'locator.dart';
+
 class LandingPage extends StatefulWidget {
-  final AuthBase authService;
-
-  const LandingPage({Key key, @required this.authService}) : super(key: key);
-
   @override
   _LandingPageState createState() => _LandingPageState();
 }
 
 class _LandingPageState extends State<LandingPage> {
   AppUser _user;
+  AuthBase authService = locator<FirebaseAuthService>();
 
   Future<void> _checkUser() async {
-    _user = await widget.authService.getCurrentUser();
+    _user = await authService.getCurrentUser();
   }
 
   void _updateUser(AppUser user) {
@@ -36,12 +36,10 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     if (_user == null) {
       return SignInPage(
-        authService: widget.authService,
         onSingIn: (User) => _updateUser(User),
       );
     } else {
       return HomePage(
-        authService: widget.authService,
         onSignOut: () => _updateUser(null),
         user: _user,
       );
