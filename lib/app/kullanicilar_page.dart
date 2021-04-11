b22c9d45bf5412cd13b13b74a66210ce95c15c35
mehmet berkay <atasoy182@gmail.com>
2021-04-11 10:03:51 +0300

    Pagination bitti.

--- lib/app/kullanicilar_page.dart
+++ lib/app/kullanicilar_page.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_lovers/app/konusma_page.dart';
 import 'package:flutter_lovers/model/app_user_model.dart';
@@ -11,7 +12,7 @@ class KullanicilarPage extends StatefulWidget {
 }
 
 class _KullanicilarPageState extends State<KullanicilarPage> {
-  List<AppUser> _tumKullanicilar = [];
+  List<AppUser> _tumKullanicilar;
   bool _isLoading = false;
   bool _hasMore = true;
   int _getirilecekElemanSayisi = 12;
@@ -43,32 +44,21 @@ class _KullanicilarPageState extends State<KullanicilarPage> {
     final _userModel = Provider.of<UserModel>(context);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Kullanıcılar"),
-        actions: [
-          FlatButton(
-              onPressed: () async {
-                await getAppUser(_enSonGetirilenUser);
-              },
-              child: Text("Next Users"))
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-              child: _tumKullanicilar.length == 0
-                  ? Center(
-                      child: Text("Kullanıcı Yok"),
-                    )
-                  : _kullaniciListesiniOlustur()),
-          _isLoading
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Container(),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Kullanıcılar"),
+          actions: [
+            FlatButton(
+                onPressed: () async {
+                  await getAppUser(_enSonGetirilenUser);
+                },
+                child: Text("Next Users"))
+          ],
+        ),
+        body: _tumKullanicilar == null
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : _kullaniciListesiniOlustur());
   }
 
   getAppUser(AppUser enSonGetirilenUser) async {
@@ -88,6 +78,7 @@ class _KullanicilarPageState extends State<KullanicilarPage> {
     QuerySnapshot _querySnapshot;
     if (enSonGetirilenUser == null) {
       print("------------ ilk 10 geliyor.");
+      _tumKullanicilar = [];
       _querySnapshot = await FirebaseFirestore.instance
           .collection("users")
           .orderBy("userName")
@@ -126,12 +117,29 @@ class _KullanicilarPageState extends State<KullanicilarPage> {
   _kullaniciListesiniOlustur() {
     return ListView.builder(
       itemBuilder: (context, index) {
+        if (index == _tumKullanicilar.length) {
+          print("**************** yeni elemanlar bekliyor");
+          return _yeniElemanlarYukleniyor();
+        }
+
         return ListTile(
           title: Text(_tumKullanicilar[index].userName),
         );
       },
-      itemCount: _tumKullanicilar.length,
+      itemCount: _tumKullanicilar.length + 1,
       controller: _scrollController,
+    );
+  }
+
+  _yeniElemanlarYukleniyor() {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Center(
+        child: Opacity(
+          opacity: _isLoading ? 1 : 0,
+          child: _isLoading ? CircularProgressIndicator() : null,
+        ),
+      ),
     );
   }
 }
